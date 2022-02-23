2142945c6659192c7cbbc89e90c1175bfeb5bb25
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-02-23 11:16:11 +0900

    시큐리티 권한 처리 완료

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.cos.security1.controller;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,44 +22,47 @@ public class IndexController {
 
     // localhost:8080/
     // localhost:8080
-    @GetMapping({"","/"})
-    public String index(){
+    @GetMapping({"", "/"})
+    public String index() {
         // mustache 기본폴더 src/main/resources/
         // viewResolver 설정 : templates(prefix), .mustache(suffix) 생략가능
         return "index"; // src/main/resources/templates/index.mustache
     }
 
     @GetMapping("/user")
-    public @ResponseBody String user(){
+    public @ResponseBody
+    String user() {
         return "user";
     }
 
     @GetMapping("/admin")
-    public @ResponseBody String admin(){
+    public @ResponseBody
+    String admin() {
         return "admin";
     }
 
     @GetMapping("/manager")
-    public @ResponseBody String manager(){
+    public @ResponseBody
+    String manager() {
         return "manager";
     }
 
     // 스프링 시큐리티 -> 해당주소를 낚아챔 - SecurityConfig 파일 생성 후 작동x
     @GetMapping("/loginForm")
-    public  String loginForm(){
+    public String loginForm() {
         return "loginForm";
     }
 
     @GetMapping("/joinForm")
-    public  String joinForm(){
+    public String joinForm() {
         return "joinForm";
     }
 
     @PostMapping("/join")
-    public String join(User user){
+    public String join(User user) {
         System.out.println(user);
         user.setRole("ROLE_USER");
-        
+
         // 비밀번호 암호화
         String rawPassword = user.getPassword();
         String encPassword = bCryptPasswordEncoder.encode(rawPassword);
@@ -67,5 +72,18 @@ public class IndexController {
         return "redirect:/loginForm";
     }
 
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody
+    String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody
+    String data() {
+        return "데이터 정보";
+    }
 
 }
