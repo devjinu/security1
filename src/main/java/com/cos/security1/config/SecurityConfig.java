69ba0d774ed3b1786e9df86bc578c3b871bd758d
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-02-23 09:45:24 +0900

    회원가입 완료

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.cos.security1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다
+    @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,6 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                 .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
                                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                                 .anyRequest().permitAll()
-                                .and().formLogin().loginPage("/login"); // 권한 없는 페이지 -> 로그인 페이지로 이동
+                                .and().formLogin().loginPage("/loginForm"); // 권한 없는 페이지 -> 로그인 페이지로 이동
     }
 }
